d21a9828d4ce355288d53ba74d2b39b2795342de
naveen-machana <naveenou@gmail.com>
2015-11-03 16:13:03 +0530

    TestCases for robot implementation

---
+++ src/main/java/org/naveen/interviews/robot/testcases/TestRobot.java
@@ -0,0 +1,52 @@
+package org.naveen.interviews.robot.testcases;
+
+//import junit.framework.Assert;
+
+//import org.junit.Test;
+import org.naveen.interviews.robot.RobotImpl;
+
+public class TestRobot {
+	// Testcase for testing the battery after 3.5 KM walk
+		//@Test
+		/*public void testWalkFor35() {
+			RobotImpl r = new RobotImpl(5, 10, 2);
+			r.walk(3.5);
+			Assert.assertEquals(30.0, r.remainingBattery());
+		}
+		
+		//@Test
+		public void testOverweight() {
+			RobotImpl r = new RobotImpl(5, 10, 2);
+			boolean isDone = r.walkWithWeight(0, 12);
+			Assert.assertEquals(false, isDone);
+			Assert.assertEquals(100.0, r.remainingBattery());
+		}
+		
+		//@Test
+		public void testLessthan15Perc() {
+			RobotImpl r = new RobotImpl(5, 10, 2);
+			r.walk(4.5);
+			r.walk(0.2);		
+		}
+		
+		//@Test
+		public void testWalk2KMWith3KG() {
+			RobotImpl r = new RobotImpl(5, 10, 2);
+			r.walkWithWeight(2, 3);
+			Assert.assertEquals(54.0, r.remainingBattery());
+		}
+		
+		//@Test
+		public void readBarCode() {
+			RobotImpl r = new RobotImpl(5, 10, 2);
+			for (int i = 0; i < 10; i++)
+				r.readBarCode(new Object());
+		}
+		
+		@Test
+		public void testReadBarcodeWalkAndWeigh() {
+			RobotImpl r = new RobotImpl(5, 10, 2);
+			r.walkWeighAndReadBarcode(2, 3, new Object());
+			Assert.assertEquals(54.0, r.remainingBattery());
+		}*/
+}
